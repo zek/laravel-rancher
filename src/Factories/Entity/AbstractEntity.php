823e46fe239794f801aa02117f63dedcae81c276
benmag@users.noreply.github.com <benmaggacis@gmail.com>
2016-05-21 01:08:05 +1000

    implement powerful setProperty method to use in AbstractEntity build method
    By default it simply set the property to the specified value
    
    But when the value is an array of objects with a `type` parameter, we
    check to see if we can instantiate an entity.
    
    We use the `type` value to decide what class to attempt to instantiate

--- src/Factories/Entity/AbstractEntity.php
+++ src/Factories/Entity/AbstractEntity.php
@@ -34,14 +34,16 @@ abstract class AbstractEntity
         }
 
         // Dynamically add additional fields
-        foreach($parameters['_fields'] as $field) {
+        if(array_key_exists('_fields', $parameters)) {
+            foreach($parameters['_fields'] as $field) {
 
-            $property = static::convertToCamelCase($field);
+                $property = static::convertToCamelCase($field);
 
-            if(!property_exists($this, $property)) {
-                $this->$property = null;
+                if(!property_exists($this, $property)) {
+                    $this->$property = null;
+                }
+
             }
-
         }
 
         $this->build($parameters);
@@ -58,10 +60,67 @@ abstract class AbstractEntity
             $property = static::convertToCamelCase($property);
 
             if (property_exists($this, $property)) {
-                $this->$property = $value;
+                $this->setProperty($property, $value);
             }
         }
     }
+
+    /**
+     * By default, simply set the property to the specified value
+     *
+     * When the value is an array of objects with a `type`
+     * parameter, check if instantiation is possible.
+     *
+     * The value of `type` is used to decide which
+     * class to attempt to instantiate
+     *
+     * @param $property
+     * @param $value
+     */
+    public function setProperty($property, $value)
+    {
+
+        $this->$property = $value;
+
+        // We've got an array, lets see if we can instantiate an entity
+        if(is_array($value) && !empty($value)) {
+
+            $array = $value; // for clarity, call it what it is
+
+            // We can only guess at what the entity might be if we have the `type`
+            if(property_exists(head($array), 'type')) {
+
+                // Get the class of the new entity we want to instantiate
+                $class = (new \ReflectionClass($this))->getNamespaceName() . "\\" . ucfirst(head($array)->type);
+
+                // Update each element of the array to have the instantiated entity
+                if(class_exists($class)) foreach($array as $key => $value) {
+
+                    // Update the array with the entity
+                    $array[$key] = new $class($value);
+
+                }
+
+                // Update the property with the array of entities
+                $this->$property = $array;
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Attempt to instantiate an entity
+     * based on the type parameter
+     *
+     * Magically build from type parameter
+     */
+    public function instantiateEntity(array $parameters)
+    {
+
+    }
+
 
     /**
      * @param string $str Snake case string
